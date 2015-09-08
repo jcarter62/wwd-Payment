9309d876db2b1eae1a563e16c834c94254a1c510
Jim Carter <jcarter62@gmail.com>
2015-09-08 11:26:07 -0700

    End of day 20150904

---
+++ SqlDB/v_CrReceipt.sql
@@ -0,0 +1,9 @@
+﻿create VIEW [dbo].[v_CrReceipt]
+AS
+SELECT        m.Id, m.DeliveryName AS mDeliveryName, m.Amount AS mAmount, m.PayRef AS mPayRef, m.PayType AS mPayType, m.PayVia AS mPayVia, m.Note AS mnote, d.Account AS dAccount, d.Name AS dName, 
+                         d.Amount AS dAmount, d.Note AS dnote, m.RcptID AS mRcptID, m.CDate AS mCreatedOn, d.Type AS dType
+FROM            dbo.CRMaster AS m INNER JOIN
+                         dbo.CRDetail AS d ON m.Id = d.CRMid
+
+GO
+
